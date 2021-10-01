fc5db43d3b45054e425108d5e4d2b585d7f8c9d5
FlomoN <flo.giger@gmail.com>
2021-10-01 21:22:26 +0200

    Open settings with animation

--- packages/frontend/src/components/Settings.tsx
+++ packages/frontend/src/components/Settings.tsx
@@ -1,7 +1,33 @@
-import react from "react";
+import react, { useState } from "react";
 import "./settings.sass";
-import icon from "./menu.svg";
+import UseAnimations from "react-useanimations";
+import menu2 from "react-useanimations/lib/menu2";
+import { useSpring, animated } from "react-spring";
 
+//TODO: Add burger icon from react-useanimations
 export default function Settings() {
-  return <div className="settings"></div>;
+  const [menuOpen, setMenuOpen] = useState(false);
+  const animatedProps = useSpring({
+    from: {
+      width: 30,
+      height: 30,
+    },
+    to: {
+      width: menuOpen ? 400 : 30,
+      height: menuOpen ? 800 : 30,
+    },
+  });
+
+  return (
+    <animated.div className="settings" style={animatedProps}>
+      <UseAnimations
+        animation={menu2}
+        size={30}
+        strokeColor="white"
+        reverse={menuOpen}
+        wrapperStyle={{ cursor: "pointer" }}
+        onClick={() => setMenuOpen(!menuOpen)}
+      />
+    </animated.div>
+  );
 }
